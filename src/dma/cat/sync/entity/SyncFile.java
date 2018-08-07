d946ed43b6d6fe7ecb5fcb21813a98e58c6dbb76
daniel marques <dani@wiris.com>
2018-08-07 12:23:49 +0200

    Added log for merge

--- src/dma/cat/sync/entity/SyncFile.java
+++ src/dma/cat/sync/entity/SyncFile.java
@@ -70,6 +70,7 @@ public class SyncFile implements Comparable {
         SyncFile e = findChildByName(f.meta.name);
         if (e!=null) {
             // Choose the best according to date
+            System.out.println(e.fullName+" ("+e.meta.modificationDate+") vs "+f.fullName+" ("+e.meta.modificationDate+")");
             if (e.meta.modificationDate<f.meta.modificationDate) {
                 e.fillFrom(f);
             }
